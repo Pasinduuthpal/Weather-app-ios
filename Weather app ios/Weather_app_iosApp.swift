6004aae5582c663038c305bd29fe245a514b9d45
pasindu <pasinduuthpal@gmail.com>
2024-02-08 01:19:01 +0530

    Initial Commit

---
+++ Weather app ios/Weather_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_app_iosApp.swift
+//  Weather app ios
+//
+//  Created by Pasindu on 2024-02-07.
+//
+
+import SwiftUI
+
+@main
+struct Weather_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
